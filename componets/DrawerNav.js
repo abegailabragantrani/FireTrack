8e71490223fe2190f99476b91976e60b38c86025
abegailabragantrani <abegailtrani26@gmail.com>
2023-11-02 01:16:58 +0800

    okay na ang ui sa drawnav

---
+++ componets/DrawerNav.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet,} from 'react-native';
+import { DrawerContentScrollView, DrawerItemList,} from '@react-navigation/drawer';
+
+
+const DrawerNav = props => {
+    return (
+        <View style={styles.container}>
+              <View style={styles.top} />
+              <View style={styles.bottom} />
+        
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 110,
+    },
+    top: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: '95%',
+      backgroundColor: '#FB9246',
+    },
+    bottom: {
+      position: 'absolute',
+      top: 10,
+      left: 0,
+      right: 0,
+      top: '10%',
+      backgroundColor: '#FFFFFF',
+    },
+   });
+
+export default DrawerNav;
--- screens/main.js
+++ screens/main.js
@@ -1,26 +1,66 @@
 import * as React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import "react-native-gesture-handler";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Account from "./drawnav/Account";
 import AboutUs from "./drawnav/AboutUs";
 import Logout from './drawnav/Logout';
 import Dashboard from './drawnav/Dashboard';
+import DrawerNav from '../componets/DrawerNav';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 
 const Drawer = createDrawerNavigator();
 
 
 export default class MainScreen extends React.Component {
-render() {
+  render() {
     return (
-        <Drawer.Navigator>
-        <Drawer.Screen name="Dashboard" component={Dashboard} />
-        <Drawer.Screen name="My Account" component={Account} />
-        <Drawer.Screen name="About Us" component={AboutUs} />
-        <Drawer.Screen name="Logout" component={Logout} />
-        </Drawer.Navigator>
-            
+      
+      <Drawer.Navigator drawerContent={props =>  <DrawerNav{...props} />}
+        screenOptions={{
+          headerShown: false,
+          drawerActiveBackgroundColor: '#FB9246',
+          drawerActiveTintColor: '#fff',
+          drawerInactiveTintColor: '#333',
+          drawerLabelStyle: {
+            marginLeft: 25,
+            fontSize: 15,
+          },
+        }}>
 
-      );
-}
+        <Drawer.Screen name="Dashboard" component={Dashboard}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon name="home" size={22} color={color} />
+            ),
+          }} />
+
+        <Drawer.Screen name="My Account" component={Account}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon name="user" size={22} color={color} />
+            ),
+          }}
+        />
+
+        <Drawer.Screen name="About Us" component={AboutUs}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon name="heart" size={22} color={color} />
+            ),
+          }} />
+
+        <Drawer.Screen name="Logout" component={Logout}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon name="logout" size={22} color={color} />
+            ),
+          }} />
+          
+      </Drawer.Navigator>
+
+
+    );
+  }
 }

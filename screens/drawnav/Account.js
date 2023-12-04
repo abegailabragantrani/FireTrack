93ebb8a5a096646f1a0032cb34963ab8716f609a
abegailabragantrani <abegailtrani26@gmail.com>
2023-12-05 00:56:19 +0800

    update profile

--- screens/drawnav/Account.js
+++ screens/drawnav/Account.js
@@ -1,25 +1,25 @@
 import * as React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
-import {TouchableOpacity } from 'react-native-gesture-handler'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native';
 
 export default function Account() {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-       <Text style={{textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 18, bottom: 170 }}>Profile</Text>
+      <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 18, bottom: 170 }}>Profile</Text>
       <View style={styles.top} />
 
       <View style={{ alignItems: 'center', justifyContent: 'center', bottom: 135, }}>
-      <Image style={styles.image}
-        source={require('../drawnav/user.png')} />
-      <Text style={{textAlign: 'center', color: '#000000', fontSize: 18, bottom: 40 }}>Abegail A. Trani</Text>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Profile')}
-        style={{ width: 200, height: 50, backgroundColor: '#171F1D', alignItems: 'center', justifyContent: 'center', borderRadius: 15, marginBottom: 15, borderWidth: 1, borderColor: '#000000' }}
-      >
-        <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>Edit Profile</Text>
-      </TouchableOpacity>
+        <Image style={styles.image}
+          source={require('../drawnav/user.png')} />
+        <Text style={{ textAlign: 'center', color: '#000000', fontSize: 18, bottom: 40 }}>Abegail A. Trani</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Profile')}
+          style={{ width: 200, height: 50, backgroundColor: '#171F1D', alignItems: 'center', justifyContent: 'center', borderRadius: 15, marginBottom: 15, borderWidth: 1, borderColor: '#000000' }}
+        >
+          <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>Edit Profile</Text>
+        </TouchableOpacity>
 
       </View>
     </View>
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 40,
     borderTopLeftRadius: 40,
   },
-  
+
   image: {
     height: 240,
     width: 180,
--- screens/profile.js
+++ screens/profile.js
@@ -1,75 +1,100 @@
 import * as React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 export default class ProfileScreen extends React.Component {
 
-render() {
-return (
-  <View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 18, bottom: 170 }}>Profile</Text>
+        <View style={styles.top} />
 
-  <View style={styles.top} />
-  <View style={styles.bottom} />
+        <View style={{ alignItems: 'center', justifyContent: 'center', bottom: 42, }}>
+          <Image style={styles.image}
+            source={require('./drawnav/user.png')} />
+          <Text style={{ textAlign: 'center', color: '#000000', fontSize: 18, bottom: 40 }}>Change Picture</Text>
 
-  <TouchableOpacity
-    onPress={() => this.props.navigation.navigate('Report')}
-    style={{ width: 200, height: 50, backgroundColor: '#FB9246', alignItems: 'center', justifyContent: 'center', bottom: 50, borderRadius: 15, marginBottom: 20, borderWidth: 1, borderColor: '#000000' }}
-  >
-    <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>Report Fire Incident</Text>
-  </TouchableOpacity>
+          <Text style={{ textAlign: 'center', right: 107, color: '#000000', fontSize: 18, bottom: 40 }}>Username</Text>
+          <TextInput
+            style={styles.fields}
+            onChangeText={(text) => {
+              this.setState({ errMsg: '' }),
+                this.setState({ username: text })
+            }
+            }
+          />
 
-  <TouchableOpacity
-    onPress={() => this.props.navigation.navigate('Incident')}
-    style={{ width: 200, height: 50, backgroundColor: '#FB9246', alignItems: 'center', justifyContent: 'center', bottom: 40, borderRadius: 15, marginBottom: 20, borderWidth: 1, borderColor: '#000000' }}
-  >
-    <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>View Incident Status</Text>
-  </TouchableOpacity>
-</View>
+          <Text style={{ textAlign: 'center', right: 120, color: '#000000', fontSize: 18, bottom: 40 }}>Adress</Text>
+          <TextInput
+            style={styles.fields}
+            onChangeText={(text) => {
+              this.setState({ errMsg: '' }),
+                this.setState({ username: text })
+            }
+            }
+          />
+
+          <Text style={{ textAlign: 'center', right: 87, color: '#000000', fontSize: 18, bottom: 40 }}>Phone Number</Text>
+          <TextInput
+            style={styles.fields}
+            onChangeText={(text) => {
+              this.setState({ errMsg: '' }),
+                this.setState({ username: text })
+            }
+            }
+          />
 
 
-  )
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Profile')}
+            style={{ width: 200, height: 50, backgroundColor: '#171F1D', alignItems: 'center', justifyContent: 'center', borderRadius: 15, marginBottom: 15, borderWidth: 1, borderColor: '#000000' }}
+          >
+            <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>Update</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+
+
+    )
+  }
 }
-}
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    color: 'white',
+    backgroundColor: '#FB9246'
   },
-  bottom: {
+  top: {
     position: 'absolute',
-    top: 280,
+    top: 200,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#ffffff',
     borderTopRightRadius: 40,
     borderTopLeftRadius: 40,
   },
-  fields: {
-    height: 50,
-    width: 300,
-    top: 31,
-    margin: 15,
-    paddingHorizontal: 40,
-    borderBottomRightRadius: 30,
-    borderBottomLeftRadius: 30,
-    borderTopRightRadius: 30,
-    borderTopLeftRadius: 30,
-    backgroundColor: '#d3d3d3d3',
 
-  },
   image: {
-    right: 10,
-    left: 25,
-    height: 350,
-    width: 550,
-    bottom: 140,
-    
+    height: 240,
+    width: 180,
+    bottom: 55
+  },
+  fields: {
+    height: 45,
+    width: 300,
+    margin: 6,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    backgroundColor: '#d3d3d3d3',
+    bottom: 55,
   },
 }
 );

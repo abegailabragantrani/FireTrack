baf30ebfc7246f157cfd06f7f2cc7effdbedb8b2
abegailabragantrani <abegailtrani26@gmail.com>
2023-12-05 00:27:23 +0800

    profile screen okay

--- screens/drawnav/Account.js
+++ screens/drawnav/Account.js
@@ -1,15 +1,27 @@
 import * as React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 import {TouchableOpacity } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native';
 
 export default function Account() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
+       <Text style={{textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 18, bottom: 170 }}>Profile</Text>
+      <View style={styles.top} />
+
+      <View style={{ alignItems: 'center', justifyContent: 'center', bottom: 135, }}>
       <Image style={styles.image}
         source={require('../drawnav/user.png')} />
-        <Text>Profile</Text>
+      <Text style={{textAlign: 'center', color: '#000000', fontSize: 18, bottom: 40 }}>Abegail A. Trani</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Profile')}
+        style={{ width: 200, height: 50, backgroundColor: '#171F1D', alignItems: 'center', justifyContent: 'center', borderRadius: 15, marginBottom: 15, borderWidth: 1, borderColor: '#000000' }}
+      >
+        <Text style={{ textAlign: 'center', color: '#ffffff', fontWeight: 'bold', fontSize: 16 }}>Edit Profile</Text>
+      </TouchableOpacity>
 
-      <View style={styles.top} />
+      </View>
     </View>
 
 
@@ -36,11 +48,9 @@ const styles = StyleSheet.create({
   },
   
   image: {
-    right: 10,
-    left: 25,
-    height: 100,
-    width: 550,
-    bottom: 100
+    height: 240,
+    width: 180,
+    bottom: 55
   },
 }
 );
